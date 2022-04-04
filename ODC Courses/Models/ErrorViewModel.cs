c646546d12c09b539d848677590d84795a9941f6
Marko <mekomedhat682@gmail.com>
2022-04-04 06:09:10 +0200

    Add project files.

---
+++ ODC Courses/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ODC_Courses.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
